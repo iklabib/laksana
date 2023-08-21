be695855d29bf296878a851a32b2b56653d78c08
Ikhwanul Labib <LJINS23003@pamapersada.net>
2023-08-21 14:58:01 +0700

    restructure build

--- build/kotlin/kotlin.py
+++ build/kotlin/kotlin.py
@@ -47,20 +47,8 @@ sha256_expected = resp.read().decode()
 
 os.system(f"aria2c --checksum=sha-256={sha256_expected} -c -j 8 -s 8 -x 8 --dir=artifacts {download_url}")
 
-# if not file_path.exists():
-#     print(f"Downloading {filename}...")
-#     os.system(f"aria2c --checksum=sha-256={sha256_expected} -c -j 8 -s 8 -x 8 --dir=artifacts {download_url}")
-# else:
-#     with open(file_path, "rb") as f:
-#         sha256_actual = hashlib.sha256(f.read()).hexdigest()
-
-#     if sha256_expected == sha256_actual:
-#         print(f"{filename} already exists and SHA-256 hash matches. Skipping.")
-#     else:
-#         print(f"{filename} SHA-256 hash does not match. Redownloading...")
-
 cwd = str(Path(__file__).parent.absolute())
 containerfile = Path(cwd) / "kotlin.containerfile"
 
 # TODO podman binding?
-os.system(f"podman build -t {cwd} -f {containerfile} --build-arg compiler_file=\"{target}\"")
+os.system(f"podman build {cwd} -t kardus:kotlin -f {containerfile} --build-arg compiler_file=\"{target}\"")
