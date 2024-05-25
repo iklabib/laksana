a4349956e7d8fa05b176dc03b9cb5c438a1b885d
Ikhwanul Labib <ikhwanul.labib@protonmail.com>
2024-05-25 14:22:05 +0700

    redo main

--- cmd/markisa/main.go
+++ cmd/markisa/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"os"
 
-	"github.com/gorilla/sessions"
-	"github.com/labstack/echo-contrib/session"
+	"codeberg.org/iklabib/markisa/containers"
+	"codeberg.org/iklabib/markisa/db"
+	"codeberg.org/iklabib/markisa/model"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-
-	routes "github.com/iklabib/markisa/routes"
 )
 
 func main() {
@@ -19,11 +20,54 @@ func main() {
 	e.Use(middleware.Gzip())
 	e.Use(middleware.CORS())
 
-	var sesskey = []byte(os.Getenv("SESSION_KEY"))
-	e.Use(session.Middleware(sessions.NewCookieStore(sesskey)))
-	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(5)))
+	client := containers.NewDefault()
+	record := db.NewRecord()
 
-	e.POST("/run", routes.Run)
+	record.CleanRecord()
+	client.CleanContainers()
+
+	exercise := db.NewExerciseDefault()
+
+	e.POST("/run", func(c echo.Context) error {
+		var req model.SubmissionRequest
+		if err := c.Bind(&req); err != nil {
+			return c.String(http.StatusBadRequest, "Bad Request")
+		}
+
+		clientId := req.User
+		
+		containerId := record.Retrieve(clientId); 
+		if containerId == "" {
+			containerId, err := client.SpawnTenant()
+			if err != nil {
+				return c.String(http.StatusInternalServerError, "Internal Error")
+			}
+
+			record.Insert(clientId, containerId)
+		}
+
+		submmission := model.Submission{
+			Type: req.Type,
+			Src: req.Src,
+			SrcTest: exercise.RetrieveTestCase(req.ExerciseId),
+		}
+
+		marshaled, err := json.Marshal(submmission)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Internal Error")
+		}
+
+		output, err := client.ExecTenant(containerId[:12], marshaled)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		unmarshal := model.RunResult{}
+		if err := json.Unmarshal(output, &unmarshal); err != nil {
+			return c.String(http.StatusInternalServerError, "Internal Error")
+		}
+		return c.JSON(http.StatusOK, unmarshal)
+	})
 
 	e.Logger.Fatal(e.Start(BASE_URL))
 }
