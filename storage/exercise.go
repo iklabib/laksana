1a6147a2c946b74bc081c670e2fbbdb4c0704764
Ikhwanul Labib <ikhwanul.labib@protonmail.com>
2024-06-05 22:39:06 +0700

    look exercises in the same directory with markisa

--- storage/exercise.go
+++ storage/exercise.go
@@ -1,31 +1,31 @@
-package db
+package storage
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"path"
 )
 
-type Exercise struct{
+type Exercise struct {
 	path string
 }
 
 func NewExerciseDefault() *Exercise {
 	exercise := Exercise{}
 	cwd, _ := os.Getwd()
-	exercise.path = path.Join(cwd, "..", "..", "exercises")
+	exercise.path = path.Join(cwd, "exercises")
 	return &exercise
 }
 
-func (ex *Exercise) RetrieveTestCase(exerciseId string) string{
+func (ex *Exercise) RetrieveTestCase(exerciseId string) (string, error) {
 	file := path.Join(ex.path, exerciseId, "test.py")
 	if _, err := os.Stat(file); os.IsNotExist(err) {
-		log.Fatalf("exercise %s does not exist\n", exerciseId)
+		return "", fmt.Errorf("exercise %s does not exist", exerciseId)
 	}
 
 	buff, err := os.ReadFile(file)
-	if (err != nil) {
-		log.Fatalf("failed to read %s test\n", exerciseId)
+	if err != nil {
+		return "", fmt.Errorf("failed to read %s test", exerciseId)
 	}
-	return string(buff)
+	return string(buff), nil
 }
