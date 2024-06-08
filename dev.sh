befa3c29529810cc76670ec253d576d2ff20ab85
Ikhwanul Labib <ikhwanul.labib@protonmail.com>
2024-06-08 18:48:42 +0700

    delete dev.sh

--- dev.sh
+++
@@ -1,7 +0,0 @@
-# TODO: make apparmor and selinux profile to fix remount issue
-docker run --rm -it -e BASE_URL=:8080 -p 8080:8080 \
-           --cap-add=sys_admin \
-           --cap-add=sys_chroot \
-           --cap-add=sys_resource \
-           --security-opt apparmor=unconfined \
-           --security-opt label=disable quay.io/iklabib/markisa
