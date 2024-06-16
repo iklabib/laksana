36b693431baa3e88f45f3ad80947a2076910926e
Ikhwanul Labib <ikhwanul.labib@protonmail.com>
2024-06-16 23:37:26 +0700

    runner

---
+++ runner/go/run.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+source "$SCRIPT_DIR/goenv.bash"
+
+main() {
+	build() {
+		go build -o /dev/null
+	}
+
+	buildTest() {
+		go test -c -o /dev/null
+	}
+
+	run() {
+		go test --short --json
+	}
+
+	if [[ "$1" == "build" ]]; then
+		build
+	elif [[ "$1" == "build-test" ]]; then
+		buildTest
+	elif [[ "$1" == "run" ]]; then
+		run
+	else
+		echo "Usage: $0 {build|build-test|run}"
+		exit 1
+	fi
+}
+
+main "$@"
