5e7654af2ab527312f0e69153bfe3396a50ed078
Ikhwanul Labib <ikhwanul.labib@protonmail.com>
2024-06-16 23:42:18 +0700

    using podman

--- chore.bash
+++ chore.bash
@@ -1,48 +1,47 @@
 #!/bin/bash
 
 main() {
-  build() {
-    clean
-    mkdir -p "build"
-    go build -o build/apparmor cmd/apparmor/main.go
-    build/apparmor
-    docker build . -t "quay.io/iklabib/markisa" -f "containerfiles/containerfile"
-  }
+	build() {
+		clean
+		go mod download
+		go mod vendor
+		mkdir -p "build"
+		go build -o build/apparmor cmd/apparmor/main.go
+		build/apparmor
+		podman build . -t "quay.io/iklabib/markisa" -f "containerfiles/containerfile"
+	}
 
-  setup() {
-    if [ "$UID" -eq 0 ]; then
-      "$@"
-    else
-      sudo cp markisa.cfg /etc/apparmor.d/markisa
-      sudo aa-enforce /etc/apparmor.d/markisa
-      sudo apparmor_parser -Kr /etc/apparmor.d/markisa
-    fi 
-  }
+	setup() {
+		if [ "$UID" -eq 0 ]; then
+			"$@"
+		else
+			sudo cp markisa.cfg /etc/apparmor.d/markisa
+			sudo aa-enforce /etc/apparmor.d/markisa
+			sudo apparmor_parser -Kr /etc/apparmor.d/markisa
+		fi
+	}
 
-  run() {
-    docker run --rm -it -p $BASE_URL:8000 \
-           --cap-add sys_admin \
-           --cap-add sys_resource \
-           --entrypoint /bin/bash \
-           --security-opt apparmor=markisa \
-           --security-opt seccomp=configs/markisa.json \
-           quay.io/iklabib/markisa
-  }
+	run() {
+		podman run --rm -it -p 8000:8000 \
+			--cap-add sys_admin \
+			--cap-add sys_resource \
+			--security-opt seccomp=profiles/seccomp/markisa.json quay.io/iklabib/markisa
+	}
 
-  clean() {
-    rm -rf build
-  }
+	clean() {
+		rm -rf build
+	}
 
-  if [[ "$1" == "build" ]]; then
-    build
-  elif [[ "$1" == "setup" ]]; then
-    setup
-  elif [[ "$1" == "run" ]]; then
-    run
-  else
-    echo "Usage: $0 {build|setup|run}"
-    exit 1
-  fi
+	if [[ "$1" == "build" ]]; then
+		build
+	elif [[ "$1" == "setup" ]]; then
+		setup
+	elif [[ "$1" == "run" ]]; then
+		run
+	else
+		echo "Usage: $0 {build|setup|run}"
+		exit 1
+	fi
 }
 
 main "$@"
