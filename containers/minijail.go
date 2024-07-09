80a78dae84cc49c0546549b45e6bd91f53d369b6
Ikhwanul Labib <ikhwanul.labib@protonmail.com>
2024-07-10 00:08:57 +0700

    sandbox with stdin

--- containers/minijail.go
+++ containers/minijail.go
@@ -3,6 +3,7 @@ package containers
 import (
 	"bytes"
 	"context"
+	"io"
 	"os/exec"
 
 	"codeberg.org/iklabib/laksana/model"
@@ -32,6 +33,25 @@ func (mn Minijail) argsBuilder(dir string, commands []string) []string {
 	// is there a way for it to look in path without bash invocation?
 	args := []string{"--config", mn.ConfigPath, "-P", dir, "--"}
 	return append(args, commands...)
+}
+
+func (mn Minijail) ExecConfinedWithStdin(dir string, commands []string, stdin io.Reader) model.SandboxExecResult {
+	args := mn.argsBuilder(dir, commands)
+
+	var stdoutBuff bytes.Buffer
+	var stderrBuff bytes.Buffer
+
+	cmd := exec.CommandContext(mn.Ctx, mn.Path, args...)
+	cmd.Stdout = &stdoutBuff
+	cmd.Stderr = &stderrBuff
+	cmd.Stdin = stdin
+	err := cmd.Run()
+
+	return model.SandboxExecResult{
+		Error:  err,
+		Stdout: stdoutBuff,
+		Stderr: stderrBuff,
+	}
 }
 
 func (mn Minijail) ExecConfined(dir string, commands []string) model.SandboxExecResult {
--- containers/sanbox.go
+++ containers/sanbox.go
@@ -1,7 +1,12 @@
 package containers
 
-import "codeberg.org/iklabib/laksana/model"
+import (
+	"io"
+
+	"codeberg.org/iklabib/laksana/model"
+)
 
 type Sandbox interface {
 	ExecConfined(dir string, commands []string) model.SandboxExecResult
+	ExecConfinedWithStdin(dir string, commands []string, stdin io.Reader) model.SandboxExecResult
 }
